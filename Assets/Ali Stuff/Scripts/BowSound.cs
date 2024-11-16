d74793457f9a13e54e39613df7d3673dbf38cde3
Aly Sharaf <ali.charaf59@gmail.com>
2024-11-16 20:17:15 +0400

    added sound effects for all towers shooting, fixed enemy not playing attack animations

--- Assets/Ali Stuff/Scripts/BowSound.cs
+++ Assets/Ali Stuff/Scripts/BowSound.cs
@@ -1,25 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BowSound : MonoBehaviour
 {
-     [SerializeField] private AudioSource bowSound; // The AudioSource for the weapon sound
+    [SerializeField] private AudioSource weaponSound; // AudioSource for the weapon sound (Bow, IceWeapon, or FireWeapon)
 
     private Animator weaponAnimator;
 
     private void Start()
     {
-        // Attempt to find an Animator on Bow, IceWeapon, or FireWeapon child objects
-        weaponAnimator = GetComponent<Animator>();  // Attach this to the same GameObject as the animator
+        // Get the Animator component from the same GameObject (e.g., Bow, IceWeapon, or FireWeapon)
+        weaponAnimator = GetComponent<Animator>();
     }
 
     // This method will be called by the animation event
     public void PlaySound()
     {
-        if (bowSound != null)
+        if (weaponSound != null)
         {
-            bowSound.Play();  // Play sound when animation reaches the trigger point
+            weaponSound.Play();  // Play the sound when the animation reaches the trigger point
         }
     }
 }
